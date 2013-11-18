b65c361da34c4bdd7bea706391bbc2c02a9d1f95
Joey Zhou <yizhou12345@gmail.com>
2013-11-18 17:23:18 -0600

    Update websitespider.py

--- websitespider.py
+++ websitespider.py
@@ -13,11 +13,8 @@ def crawl(url, depth):
 	if depth<=0:
 		return 1
 	if(depth >0):
-		print("RECURSION DEPTH : " + str(depth))
 		print("NOW CRAWLING : "+str(url))
-		#print('')
-		
-		#Grab files
+		#Grab files at root URL
 		grab_files.grab_files(url)
 
 		#Recursively visit next tree level
@@ -31,19 +28,15 @@ def crawl(url, depth):
 
 		for entry in links:
 			child_url = str(urljoin(str(entry.base_url),str(entry.url)))
-
 			#ignore anchor tags
 			if child_url.find('#')!=-1:
 				child_url=str(child_url[0:child_url.find('#')+1])
-			#print("CHILD URL : " + child_url)
-
 			if child_url not in url_set:
 				url_set.add(child_url)
 				print("2DEPTH TYPE: " + str(type(depth)) + " DEPTH: " + str(depth))
 				#print ("EMBARKING ON " + str(child_url))
 				depth = crawl(child_url, depth-1)
 		return depth+1	
-
 
 url_set.clear()
 crawl ('http://courses.engr.illinois.edu/cs433/', 3)
